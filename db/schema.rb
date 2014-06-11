c4f5a0ad7836921c930da66e3b352b1c0ce743e6
Gabyshev Semen <gabyshev.sa@gmail.com>
2014-06-11 11:48:23 +0700

    devise authentification added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610145541) do
+ActiveRecord::Schema.define(version: 20140610152850) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -22,5 +22,17 @@ ActiveRecord::Schema.define(version: 20140610145541) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.string   "username"
+    t.string   "role"
+    t.datetime "remember_created_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
