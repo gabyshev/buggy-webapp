e36cbef69519ac101e6585d6616aa5fca9e685e4
Gabyshev Semen <gabyshev.sa@gmail.com>
2014-06-14 14:38:32 +0700

    paperclip added (product image)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  has_attached_file :image, styles: {medium: '300x300>', thumb: '350x350>'}, default_url: 'http://placehold.it/350x250'
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610152850) do
+ActiveRecord::Schema.define(version: 20140614064438) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20140610152850) do
     t.boolean  "preowned"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: true do |t|
