5ee52acd8d048f06c761c11f7b94292114a46d81
Gabyshev Semen <gabyshev.sa@gmail.com>
2014-06-15 22:41:47 +0700

    authorization bug

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.role?(:admin)
-      can :manage, :all
+      can :destroy, :all
     else
       can :read, :all
       can :update, Product do
