127d4c20e7fbabed6749d9e91d9e6bb3be65a45a
Gabyshev Semen <gabyshev.sa@gmail.com>
2014-06-16 19:20:12 +0700

    switch to postgresql database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140614091804) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
@@ -38,6 +41,6 @@ ActiveRecord::Schema.define(version: 20140614091804) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
