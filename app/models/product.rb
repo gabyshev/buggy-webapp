ef74938af655e7f99c93843d913babdbb7cc0658
Gabyshev Semen <gabyshev.sa@gmail.com>
2014-06-19 16:31:59 +0700

    BUG: limit to amount of products 5

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ActiveRecord::Base
   has_attached_file :image, styles: {medium: '300x300>', thumb: '350x350>'}, default_url: 'http://placehold.it/350x250'
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   belongs_to :user
+  validate :user_cannot_have_more_than_five_products
+
+  def user_cannot_have_more_than_five_products
+    errors.add(:base, "You can't have more than 5 products") if self.user.products.count >= 5
+  end
 end
