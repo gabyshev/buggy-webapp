3d5db2a915bba1ce43debcfa1ede77dfdd247007
Gabyshev Semen <gabyshev.sa@gmail.com>
2014-06-19 16:40:02 +0700

    BUG: price validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
-  has_attached_file :image, styles: {medium: '300x300>', thumb: '350x350>'}, default_url: 'http://placehold.it/350x250'
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   belongs_to :user
+  has_attached_file :image, styles: {medium: '300x300>', thumb: '350x350>'}, default_url: 'http://placehold.it/350x250'
+
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
   validate :user_cannot_have_more_than_five_products
 
   def user_cannot_have_more_than_five_products
